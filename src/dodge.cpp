8e3b0adb7df914b3625aeea741cca327e84ba60d
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-10-04 02:53:02 +0100

    Update dodge.cpp

--- src/dodge.cpp
+++ src/dodge.cpp
@@ -1147,7 +1147,7 @@ void dodge::react_to_bash_sprint(RE::Actor* a_attacker, float attack_range, floa
 					auto time = distance/mov_speed;
 					auto time_needed = dodge::GetSingleton()->GenerateRandomFloat(0.0f, dodge::round_to(time, 10.0, 1.0));
 
-					if (time_needed <= 0.015){
+					if (time_needed <= 0.015f){
 						dodge::GetSingleton()->BashSprint_attempt_dodge(refr, &dodge_directions_tk_horizontal, mov_speed);
 
 					}else{
@@ -1298,7 +1298,7 @@ void dodge::react_to_shouts_spells(RE::Actor* a_attacker, float attack_range, fl
 					auto time_needed = dodge::GetSingleton()->GenerateRandomFloat(0.0f, dodge::round_to(time, 10.0, 1.0));
 
 					if (time_needed <= 0.015f){
-						time_needed == 0.0f ? dodge::GetSingleton()->Shouts_Spells_attempt_dodge(refr, &dodge_directions_tk_reactive, attack_speed) 
+						time_needed <= 0.0f ? dodge::GetSingleton()->Shouts_Spells_attempt_dodge(refr, &dodge_directions_tk_reactive, attack_speed) 
 						: dodge::GetSingleton()->Shouts_Spells_attempt_dodge(refr, &dodge_directions_tk_horizontal, attack_speed);
 
 					}else{
